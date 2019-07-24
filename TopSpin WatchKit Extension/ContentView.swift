ea6e27f1f21e4a1b422e1a8f0932eb6dfae3497b
willbrandin <will@tidelaundry.com>
2019-07-24 15:28:05 -0500

    Initial Commit

---
+++ TopSpin WatchKit Extension/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  TopSpin WatchKit Extension
+//
+//  Created by Will Brandin on 7/24/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ TopSpin WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  TopSpin WatchKit Extension
+//
+//  Created by Will Brandin on 7/24/19.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
